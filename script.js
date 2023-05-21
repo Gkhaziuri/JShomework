a4edee4654540803d88486dc4a43ba498d9b5086
Giorgi Khaziuri <gxaziuri7@gmail.com>
2023-05-21 19:01:10 +0400

    added events in js

---
+++ script.js
@@ -0,0 +1,24 @@
+var firstName = "Giorgi";
+var lastName = "Khaziuri";
+var age = "21";
+var hobby = "football";
+var workplace = "Majorel";
+
+// console.log(`Hello, my name is ${firstName} ${lastnName}. I'm ${age} years old. My hobby is ${hobby} and I work in ${workplace}.`)
+
+console.log("Hello, my name is " + firstName + " " + lastName + ". I'm " + age + " years old. My hobby is " + hobby + " and I work at " + workplace + ".");
+
+
+let x = 15;
+let y = 55;
+let z = 25;
+let w = 28;
+
+console.log(x * y / z + w)
+
+let a  = 5;
+let b = 12;
+
+console.log("S =" +" "+(a*b))
+console.log("Diagonal of the rectangle is")
+console.log(a*a+b*b)
